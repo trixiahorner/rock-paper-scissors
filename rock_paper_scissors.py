c3f1210311bf465042d5e10de082d8ea69920f35
Trixia Horner <162903587+trixiahorner@users.noreply.github.com>
2024-05-07 20:59:36 -0400

    Add files via upload

---
+++ rock_paper_scissors.py
@@ -0,0 +1,51 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+import random
+rpc = [rock, paper, scissors]
+
+user_choice = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. ")
+print (rpc[int(user_choice)])
+
+print("Computer chose:")
+computer_choice = random.randint(0,2)
+print(rpc[computer_choice])
+
+if int(user_choice) == computer_choice:
+  print("It's a tie")
+elif int(user_choice) == 0 and computer_choice == 1:
+  print("Paper beats rock. You lose.")
+elif int(user_choice) == 0 and computer_choice == 2:
+  print("Rock beats scissors. You win.")
+elif int(user_choice) == 1 and computer_choice == 0:
+  print("Paper beats rock. You win.")
+elif int(user_choice) == 1 and computer_choice == 2:
+  print("Scissors beats paper. You lose.")
+elif int(user_choice) == 2 and computer_choice == 0:
+  print("Rock beats scissors. You lose.")
+elif int(user_choice) == 2 and computer_choice == 1:
+  print("Scissors beats paper. You win.")
